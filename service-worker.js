343db80b9b0e7f7107b49c65932a4800ac2734d1
lesliecordier <lesly_s83@msn.com>
2017-03-15 10:52:06 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -20,7 +20,6 @@ var REQUIRED_FILES = [
     'docs/M-CHAPOUTIER-LA-BERNARDINE-Rouge.pdf',
     'docs/clos-real.pdf',
     'images/cepage.jpg',
-    'images/vignoble.jpg',
     'images/verres-vins.jpg',
     'images/up.png',
     'images/home.png',
