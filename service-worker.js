edd06407a05b8b1dff393d1abdec61ea260a716f
Leslie <Leslie@localhost>
2017-03-16 11:10:34 +0100

    Initial

--- service-worker.js
+++ service-worker.js
@@ -86,7 +86,7 @@ function fromCache(request) {
 function update(request) {
     return caches.open(CACHE_NAME).then(function (cache) {
         return fetch(request).then(function (response) {
-          console.log('REPONSE: '+ response);
+          request.waitUntil(console.log('REPONSE: '+ response));
           return cache.put(request, response);
         });
   });
