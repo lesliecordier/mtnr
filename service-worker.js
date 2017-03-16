9dfbbd571302b622e1cc4bc1765a7ce65242e8d0
Leslie <Leslie@localhost>
2017-03-16 10:52:28 +0100

    Initial

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,6 @@ var REQUIRED_FILES = [
     'docs/clos-real.pdf',
     'images/cepage.jpg',
     'images/verres-vins.jpg',
-    'images/vignoble.jpg',
     'images/up.png',
     'images/home.png',
     'images/fond-accueil.jpg',
